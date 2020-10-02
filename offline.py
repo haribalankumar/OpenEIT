1c332894f0a712a950557611287b8aa8d510bece
andrew <andrew.s.creegan@gmail.com>
2020-10-02 15:35:35 +1300

    Simplified offline.py (Based on sam's offline.py which creates an animation of multiple lines of data)
    Added some example data

--- offline.py
+++ offline.py
@@ -3,14 +3,79 @@
 # Copyright (c) Mindseye Biomedical LLC. All rights reserved.
 # Distributed under the (new) CC BY-NC-SA 4.0 License. See LICENSE.txt for more info.
 
-	Read in a data file and plot it using an algorithm. 
+    Read in a data file and plot it using an algorithm.
 
 """
 from __future__ import division, absolute_import, print_function
-import numpy as np
+import matplotlib.animation as animation
 import matplotlib.pyplot as plt
-import OpenEIT.dashboard
-import OpenEIT.reconstruction 
+import numpy as np
+import OpenEIT.reconstruction
+import os
+
+n_electrodes = 16
+data_directory = "data/"
+results_directory = "results/"
+data_filename = data_directory + "200928lungs_working4.txt"
+baseline_filename = data_directory + "200928NoWire.txt"
+results_filename = "results/200928lungs_working45.gif"
+
+
+def main():
+    # Create a pyEIT Jacobian Reconstruction Object
+    pyeit_obj = OpenEIT.reconstruction.JacReconstruction(n_electrodes)
+
+    # Get mesh plotting variables
+    electrode_indices = pyeit_obj.el_pos
+    pts = pyeit_obj.mesh_obj['node']
+    tri = pyeit_obj.mesh_obj['element']
+    x = pts[:, 0]
+    y = pts[:, 1]
+
+    # Load Data
+    data = load_data(data_filename)
+    baseline_data = load_data(baseline_filename)
+
+    # First reconstruction call sets baseline
+    pyeit_obj.eit_reconstruction(baseline_data[0])
+
+    plot_images = []
+    fig, ax = plt.subplots(figsize=(6, 4))
+    for frame in data:
+        # EIT Reconstruction
+        eit_image = pyeit_obj.eit_reconstruction(frame)
+
+        # Create plot of reconstruction
+        plot_image = ax.tripcolor(x, y, tri, eit_image, vmin=-0, vmax=50000)
+        plot_images.append([plot_image])
+
+    # Plot images
+    ax.plot(x[electrode_indices], y[electrode_indices], 'ro')
+    for i, e in enumerate(electrode_indices):
+        ax.text(x[e], y[e], str(i + 1), size=12)
+    ax.axis('equal')
+    fig.colorbar(plot_images[-1][0])
+
+    # Save animation as gif
+    writer_gif = animation.PillowWriter(fps=30)
+    ani = animation.ArtistAnimation(fig, plot_images, interval=200, blit=True, repeat_delay=500)
+    if not os.path.exists(results_directory):
+        os.mkdir(results_directory)
+    ani.save(results_filename, writer_gif)
+
+    plt.show()
+
+
+def load_data(file_name):
+    with open(file_name, "r") as f:
+        lines = f.readlines()
+
+    data = []
+    for line in lines:
+        data.append(parse_line(line))
+
+    return data
+
 
 def parse_line(line):
     try:
@@ -28,76 +93,6 @@ def parse_line(line):
             return None
     return np.array(items)
 
-n_el = 32
-""" Load Data: select a file you have created by simdata.py, or recorded through the dashboard """
-text_file = open("rawdata8.txt", "r")
-lines       = text_file.readlines()
-print ("length lines: ",len(lines))
-# This is the baseline image.
-f0          = parse_line(lines[4]).tolist()
-# this is the new difference image. 
-f1          = parse_line(lines[5]).tolist()
 
-""" Select one of the three methods of EIT tomographic reconstruction, Gauss-Newton(Jacobian), GREIT, or Back Projection(BP)"""
-# This is the Gauss Newton Method for tomographic reconstruction. 
-g = OpenEIT.reconstruction.JacReconstruction(n_el=n_el)
-# Note: Greit method uses a different mesh, so the plot code will be different.
-# g = OpenEIT.reconstruction.GreitReconstruction(n_el=n_el)
-# 
-#g = OpenEIT.reconstruction.BpReconstruction(n_el=n_el)
-
-data_baseline = f0
-print ('f0',len(f0),len(f1))
-g.update_reference(data_baseline)
-# set the baseline. 
-baseline = g.eit_reconstruction(f0)
-# do the reconstruction. 
-difference_image = g.eit_reconstruction(f1)
-#print (difference_image)
-# #print(g.__dict__)
-
-
-mesh_obj = g.mesh_obj
-el_pos = g.el_pos
-ex_mat = g.ex_mat
-pts     = g.mesh_obj['node']
-tri = g.mesh_obj['element']
-x   = pts[:, 0]
-y   = pts[:, 1]
-
-""" Uncomment the below code if you wish to plot the Jacobian(Gauss-Newton) or Back Projection output. Also, please look at the pyEIT documentation on how to optimize and tune the algorithms. A little tuning goes a long way! """
-# JAC OR BP RECONSTRUCTION SHOW # 
-fig, ax = plt.subplots(figsize=(6, 4))
-im = ax.tripcolor(x,y, tri, difference_image,
-                  shading='flat', cmap=plt.cm.gnuplot)
-ax.plot(x[el_pos], y[el_pos], 'ro')
-for i, e in enumerate(el_pos):
-    ax.text(x[e], y[e], str(i+1), size=12)
-ax.axis('equal')
-fig.colorbar(im)
-plt.show()
-
-
-""" Uncomment the below code if you wish to plot the GREIT output. Also, please look at the pyEIT documentation on how to optimize and tune the algorithms. A little tuning goes a long way! """
-# GREIT RECONSTRUCION IMAGE SHOW # 
-# new     = difference_image[np.logical_not(np.isnan(difference_image))]
-# flat    = new.flatten()
-# av      = np.median(flat)
-# total   = []
-# for i in range(32):
-#     for j in range(32):
-#         if difference_image[i,j] < -5000: 
-#             difference_image[i,j] = av
-
-# print ('image shape: ',difference_image.shape)
-# fig, ax = plt.subplots(figsize=(6, 4))
-# #rotated = np.rot90(image, 1)
-# im = ax.imshow(difference_image, interpolation='none', cmap=plt.cm.rainbow)
-# fig.colorbar(im)
-# ax.axis('equal')
-# ax.set_title(r'$\Delta$ Conductivity Map of Lungs')
-# fig.set_size_inches(6, 4)
-# # fig.savefig('../figs/demo_greit.png', dpi=96)
-# plt.show()
-
-
+if __name__ == "__main__":
+    main()
